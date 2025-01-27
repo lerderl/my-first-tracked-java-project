61ec08ef9f48bb2496113aca5b2556ab3d9972f2
lerderl <josepholukunle1107@gmail.com>
2025-01-27 12:07:35 +0100

    Clean up code

--- src/ObjectReferenceApp.java
+++ src/ObjectReferenceApp.java
@@ -3,33 +3,35 @@ public class ObjectReferenceApp {
     DodgeChallenger redDodgeChallenger = new DodgeChallenger();
     DodgeChallenger greenDodgeChallenger = new DodgeChallenger();
 
-    Byte numberOfSeats = 5;
-    Short horsePower = 492;
-    Integer price = 29999;
-    Long registrationNumber = 3981298793721L;
+    System.out.println(redDodgeChallenger);
+    System.out.println(greenDodgeChallenger);
 
-    Float fuelConsumptionCombined = 15.5F;
-    Double fuelConsumptionPriceAverage = 15.2312513451;
+    byte numberOfSeats = 5;
+    short horsePower = 492;
+    int price = 29999;
+    long registrationNumber = 3981298793721L;
 
-    Boolean isDamaged = true;
-    Character energyEfficiencyCategory = 'G';
+    float fuelConsumptionCombined = 15.5F;
+    double fuelConsumptionPriceAverage = 15.2312513451;
+
+    boolean isDamaged = true;
+    char energyEfficiencyCategory = 'G';
 
     String carName = "Dodge Challenger SRT 392";
-    String carNameNew = new String("Dodge Challenger SRT 392");
+    String carNameNew = "Dodge Challenger SRT 492";
 
     System.out.println("Number of seats: " + numberOfSeats);
     System.out.println("Horsepower: " + horsePower);
-    System.out.println("Price: $" + price.floatValue());
+    System.out.println("Price: $" + (float) price);
     System.out.println("Registration Number: " + registrationNumber);
     System.out.println("Combined fuel consumption: " + fuelConsumptionCombined + "l/100km");
-    System.out.println("Price average: $" + fuelConsumptionPriceAverage.intValue());
+    System.out.println("Price average: $" + (int) fuelConsumptionPriceAverage);
     System.out.println("This car is damaged: " + isDamaged);
     System.out.println("Energy efficiency category: " + energyEfficiencyCategory);
 
     System.out.println("Car name: " + carName);
     System.out.println("Car name with uppercase: " + carName.toUpperCase());
     System.out.println("Car name with lowercase: " + carName.toLowerCase());
-    System.out.println("Car name and car name with new keyword are equals: " + carName.equals(carNameNew));
     System.out.println("New car name: " + carNameNew);
   }
 }
