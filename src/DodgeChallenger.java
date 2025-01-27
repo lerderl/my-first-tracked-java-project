55573f7dc6613a984ec8e28ed6cf711214c7d287
lerderl <josepholukunle1107@gmail.com>
2025-01-27 11:26:52 +0100

    Object reference

---
+++ src/DodgeChallenger.java
@@ -0,0 +1,8 @@
+public class DodgeChallenger {
+    DodgeChallenger redDodgeChallenger = newDodgeChallenger("red");
+    DodgeChallenger greenDodgeChallenger = newDodgeChallenger("green");
+
+    private DodgeChallenger newDodgeChallenger(String red) {
+        return null;
+    }
+}
